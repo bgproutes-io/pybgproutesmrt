26e114f6887974c522aa4fa379a35bdebc739c88
thomas alfroy <thomas.alfroy@etu.unistra.fr>
2025-09-17 19:45:55 +0200

    add some compilation fixes

---
+++ c_mrt_parser/clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf *.o
+rm -rf *.a
+rm -rf *.status
+rm -rf *.log
+rm -rf bgproutesmrt-config.h.in
+rm -rf bgproutesmrt-config.h
+rm -rf Makefile
+rm -rf configure
+rm -rf bgpgill
+rm -rf bgpgill.dSYM
+rm -rf autom4te.cache
+
--- c_mrt_parser/main.c
+++ c_mrt_parser/main.c
@@ -4,8 +4,7 @@
  * SPDX-License-Identifier: GPL-2.0-only
  */
 
-#include <file_buffer.h>
-#include <mrt_entry.h>
+#include "file_buffer.h"
 
 
 
